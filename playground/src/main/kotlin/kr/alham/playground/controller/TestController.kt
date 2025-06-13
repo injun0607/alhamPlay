e5819d0cff7626ce6f6821ebce639aa36c90826b
injun0607 <injun0607@mydongsim.com>
2025-06-13 12:22:23 +0900

    몬스터 드랍시스템 2일차 / 인벤토리 시스템 작업 1일차

---
+++ playground/src/main/kotlin/kr/alham/playground/controller/TestController.kt
@@ -0,0 +1,123 @@
+package kr.alham.playground.controller
+
+import jakarta.annotation.PostConstruct
+import kr.alham.playground.domain.inventory.EquipmentInventory
+import kr.alham.playground.domain.inventory.MaterialInventory
+import kr.alham.playground.domain.inventory.MaterialInventoryItem
+import kr.alham.playground.domain.item.Equipment
+import kr.alham.playground.domain.item.Material
+import kr.alham.playground.domain.player.Player
+import kr.alham.playground.repository.inventory.EquipmentItemInventoryRepository
+import kr.alham.playground.repository.inventory.MaterialItemInventoryRepository
+import kr.alham.playground.repository.item.EquipmentRepository
+import kr.alham.playground.repository.item.MaterialRepository
+import kr.alham.playground.service.inventory.InventoryService
+import kr.alham.playground.service.player.PlayerService
+import org.springframework.stereotype.Controller
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Transactional
+class TestController(
+    private val inventoryService: InventoryService,
+    private val playerService: PlayerService,
+    private val equipmentItemInventoryRepository: EquipmentItemInventoryRepository,
+    private val materialItemInventoryRepository: MaterialItemInventoryRepository,
+    private val equipmentRepository: EquipmentRepository,
+    private val materialRepository: MaterialRepository
+) {
+
+
+
+    @PostConstruct
+    fun init(){
+
+        var equipmentOne : Equipment = Equipment(
+            name = "TestEquipment",
+            description = "Test Equipment Description"
+        )
+        var equipmentTwo : Equipment = Equipment(
+            name = "TestEquipment2",
+            description = "Test Equipment Description 2"
+        )
+        var equipmentThree : Equipment = Equipment(
+            name = "TestEquipment3",
+            description = "Test Equipment Description 3"
+        )
+
+        var materialOne: Material = Material(
+            name = "TestMaterial",
+            description = "Test Material Description"
+        )
+
+        var materialTwo: Material = Material(
+            name = "TestMaterial2",
+            description = "Test Material Description 2"
+        )
+
+        var materialThree: Material = Material(
+            name = "TestMaterial3",
+            description = "Test Material Description 3"
+        )
+
+        val player = playerService.savePlayer(
+            Player(
+                name = "TestPlayer",
+            )
+        )
+
+        equipmentItemInventoryRepository.save(
+            EquipmentInventory(
+                player = player
+            )
+        )
+
+        materialItemInventoryRepository.save(
+            MaterialInventory(
+                player = player
+            )
+        )
+
+        equipmentRepository.save(
+            equipmentOne
+        )
+
+        equipmentRepository.save(
+            equipmentTwo
+        )
+
+        equipmentRepository.save(
+            equipmentThree
+        )
+
+        materialRepository.save(
+            materialOne
+        )
+        materialRepository.save(
+            materialTwo
+        )
+        materialRepository.save(
+            materialThree
+        )
+
+
+    }
+
+    @GetMapping("/test")
+    fun inventoryTest(): Material {
+
+        val material = materialRepository.findById(1L).orElseThrow()
+        println(inventoryService.getMaterialInventoryByPlayerId(1L).materialItemList.size)
+        inventoryService.saveItemToPlayerInventory(1L,material)
+        println(inventoryService.getMaterialInventoryByPlayerId(1L).materialItemList.size)
+        return inventoryService.getMaterialInventoryByPlayerId(1L).materialItemList[0].material
+    }
+
+    @GetMapping("/test/material")
+    fun materialTest(): Material {
+        return inventoryService.getMaterialInventoryByPlayerId(1L).materialItemList[0].material
+    }
+
+}
---
+++ playground/src/main/kotlin/kr/alham/playground/dto/inventory/PlayerInventoryDTO.kt
@@ -0,0 +1,8 @@
+package kr.alham.playground.dto.inventory
+
+data class PlayerInventoryDTO(
+    val playerId: Long? = null,
+    //materialInventory
+    //equipmentInventory
+) {
+}
